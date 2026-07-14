0b75165e146d6f387cae189434a432ed945cacaf
sultonov <sultonovzerifboy@gmail.com>
2026-07-14 14:24:49 +0500

    lazygit: fix invalid config var, set border highlight via nvim_set_hl

--- dot_config/nvim/lua/plugins/lazygit.lua
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -8,12 +8,13 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  -- Config via vim globals (plugin has no setup() function)
+  -- Config via vim.g globals (plugin has no setup() function)
   init = function()
     vim.g.lazygit_floating_window_winblend = 3
     vim.g.lazygit_floating_window_scaling_factor = 0.95
-    vim.g.lazygit_floating_window_corner_color = "#b4befe"
     vim.g.lazygit_floating_window_use_alpha = true
     vim.g.lazygit_show_hidden_files = true
+    -- Set border highlight to lavender
+    vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = "#b4befe", default = true })
   end,
 }
